4071ba6436de55abf7a6b534d52bde1ddbedf21e
Navid <navid.naseri.079@gmail.com>
2021-12-02 17:52:45 +0330

    Better Error Handling

--- commands/create.py
+++ commands/create.py
@@ -5,11 +5,13 @@ cwd = os.getcwd()
 files = filter(lambda f: re.match(r'.*\.mp3|.*\.Mp3|.*\.mP3|.*\.MP3', f), os.listdir(cwd))
 
 def create(command = []):
-  if command[0].lower() == 'list':
-    createList()
-  else:
-    raise SyntaxError('Invalid Command - See create -h for help')
-
+  try:
+    if command[0].lower() == 'list' and len(command) == 1:
+      createList()
+    else:
+      raise Exception()
+  except:
+      raise SyntaxError('Invalid Command - See create -h for help')
 
 def createList():
   print('Creating List...')
