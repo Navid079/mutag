d09da5872ac7af99e1624aca9605c2febed6231d
Navid <navid.naseri.079@gmail.com>
2021-12-03 02:47:50 +0330

    create list added

--- package/commands/create.py
+++ package/commands/create.py
@@ -1,7 +1,13 @@
 import os
 import re
 
-cwd = os.getcwd()
+from ..settings import settings
+
+cwd = settings.cwd
+mutag = settings.mutag
+tty = settings.stdscr
+print = settings._print
+
 files = filter(lambda f: re.match(r'.*\.mp3|.*\.Mp3|.*\.mP3|.*\.MP3', f), os.listdir(cwd))
 
 def create(command = []):
@@ -11,11 +17,13 @@ def create(command = []):
     else:
       raise Exception()
   except:
-      raise SyntaxError('Invalid Command - See create -h for help')
+      raise SyntaxError('Invalid Command - See create -h for help\n')
 
 def createList():
-  print('Creating List...')
-  print(f'Working Directory: {cwd}')
-  print(f'Listing Files:')
-  for file in files:
-    print(f'{file} found')
+  print('Creating List...\n')
+  print(f'Working Directory: {cwd}\n')
+  print(f'Listing Files:\n')
+  with open(os.path.join(mutag, 'list.txt'), 'w') as lst:
+    for i, file in enumerate(files):
+      print(f'{file} found\n')
+      lst.write(f'{file}:{i}')
